d16c181518dc46ebe92c3261cc613933ba097efe
EmmaZhang <emmazxd@gmail.com>
2024-03-06 17:04:40 +1100

    fix inspireMe bug

--- controllers/inspireMe.js
+++ controllers/inspireMe.js
@@ -2,7 +2,8 @@ const axios = require("axios");
 
 async function renderPage(req, res) {
   try {
-    res.render("inspireMe", { recipes: [] });
+    var msg = "";
+    res.render("inspireMe", { recipes: [], msg });
   } catch (error) {
     console.error("Error rendering page:", error);
     res.status(500).send("Error rendering page");
@@ -18,9 +19,16 @@ async function searchRecipe(req, res) {
     );
     // Extract recipe "meals" object from response
     var recipes = response.data.meals;
-    // only display 3 result
-    recipes = recipes.slice(0, 3);
-    res.render("inspireMe", { recipes: recipes || [] });
+
+    if (!recipes || recipes.length === 0) {
+      var msg = "No Recipes yet";
+      res.render("inspireMe", { recipes: [], msg });
+    } else {
+      msg = "";
+      // only display 3 result
+      recipes = recipes.slice(0, 3);
+      res.render("inspireMe", { recipes, msg });
+    }
   } catch (error) {
     console.error("Error fetching recipe:", error);
     res.status(500).send("Error fetching recipe");
