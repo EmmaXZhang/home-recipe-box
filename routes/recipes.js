7420d09e90ab53725447c7a63b624c4bbac76609
EmmaZhang <emmazxd@gmail.com>
2024-03-06 11:07:17 +1100

    change css for edit page,add timestamp on review

--- routes/recipes.js
+++ routes/recipes.js
@@ -2,27 +2,33 @@ const express = require("express");
 const router = express.Router();
 const recipesController = require("../controllers/recipes");
 const upload = require("../utilities/multer");
+const ensureLoggedIn = require("../config/ensureLoggedIn");
 
 // GET /recipes/
 router.get("/", recipesController.index);
 
 // GET /recipes/new
-router.get("/new", recipesController.new);
+router.get("/new", ensureLoggedIn, recipesController.new);
 
 // POST /recipes/
-router.post("/", upload.single("image"), recipesController.create);
+router.post(
+  "/",
+  upload.single("image"),
+  ensureLoggedIn,
+  recipesController.create
+);
 
 // GET /recipes/:id
 router.get("/:id", recipesController.show);
 
 // GET /recipes/:id
-router.get("/:id/edit", recipesController.edit);
+router.get("/:id/edit", ensureLoggedIn, recipesController.edit);
 
 // PUT /recipes/:id
-router.put("/:id", recipesController.update);
+router.put("/:id", ensureLoggedIn, recipesController.update);
 
 // Delet /recipes/:id
-router.delete("/:id", recipesController.delete);
+router.delete("/:id", ensureLoggedIn, recipesController.delete);
 
 // POST /recipes/search
 router.post("/search", recipesController.search);
--- routes/shoppinglists.js
+++ routes/shoppinglists.js
@@ -1,20 +1,21 @@
 const express = require("express");
 const router = express.Router();
 const shoppinglistController = require("../controllers/shoppinglists");
+const ensureLoggedIn = require("../config/ensureLoggedIn");
 
 // GET /shoppinglists
 router.get("/", shoppinglistController.index);
 
 //create a new shoping list
-router.post("/", shoppinglistController.create);
+router.post("/", ensureLoggedIn, shoppinglistController.create);
 
 //GET /shoppinglists/:id
 router.get("/:id", shoppinglistController.show);
 
 // DELETE /shoppinglists/:id
-router.delete("/:id", shoppinglistController.delete);
+router.delete("/:id", ensureLoggedIn, shoppinglistController.delete);
 
 // PUT /shoppinglists/:id
-router.put("/:id", shoppinglistController.update);
+router.put("/:id", ensureLoggedIn, shoppinglistController.update);
 
 module.exports = router;
